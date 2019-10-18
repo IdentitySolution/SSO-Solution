836bf4b0c611c29729dd1ac5633532bf0cffa8c0
Daniel Carrera <daniel.r.carrera@outlook.com>
2019-10-18 19:45:10 -0400

    Moved scripts to Scripts folder

--- Scripts/CallAddHosts.cmd
+++ Scripts/CallAddHosts.cmd
@@ -2,4 +2,5 @@
 ECHO Running CallAddHosts
 
 set hosts="website.com identity.website.com api.website.com mvc-implicit.website.com mvc-hybrid.website.com js.website.com react.website.com"
+CD .\Scripts
 .\AddHosts.cmd %hosts%
