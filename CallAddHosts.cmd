af7b7e2a96231831d48dc0f4c583a8a8a2fc254d
Daniel Carrera <daniel.r.carrera@outlook.com>
2019-10-18 17:14:42 -0400

    Added api.website.com

--- CallAddHosts.cmd
+++ CallAddHosts.cmd
@@ -1,5 +1,5 @@
 @echo off
 ECHO.
 
-set hosts="website.com identity.website.com mvc-implicit.website.com mvc-hybrid.website.com js.website.com react.website.com"
+set hosts="website.com identity.website.com api.website.com mvc-implicit.website.com mvc-hybrid.website.com js.website.com react.website.com"
 .\AddHosts.cmd %hosts%
